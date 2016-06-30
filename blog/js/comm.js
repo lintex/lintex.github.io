b85f5868347992f28f8c381e8c3b0d43eb066f47
无辜的微笑 <ceo@ixxj.com>
2016-06-30 17:37:36 +0800

    诸多更新

--- blog/js/comm.js
+++ blog/js/comm.js
@@ -64,21 +64,51 @@ $(document).ready(function(){
 			}
 		});
 
-
-		// $.ajax({ 
-		// 	type: "post", 
-		// 	url : "/cc.php", 
-		// 	data: 'commContent='+content+'&commAuthor='+commAuthor+'&ID='+ID,
-		// 	error: function(){
-		// 		alert('Sorry，出错了！');
-		// 		$("#commContent").val(content);
-		// 	},
-		// 	success: function(){
-		// 		$("#comments").prepend("<li><b>"+commAuthor+"</b>："+content+"<br><i>"+dd()+"</i></li>");
-		// 		$("#cc").html(parseInt($("#cc").text())+1);
-		// 		$("#commContent").val("");
-		// 	} 
-		// });
 		$("#send").attr("disabled",false);
 	});
 });
+
+
+		window.onload = function(){
+	    var obtn = document.getElementById('returnTop');
+		//获取页面可视区的高度
+		var clientHeight = document.documentElement.clientHeight;
+		var timer = null;
+		var isTop = true;
+
+		//滚动条滚动时触发
+		window.onscroll = function(){
+			var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+			if (osTop >= clientHeight){
+				obtn.style.display = 'block';
+			}else{
+				obtn.style.display = 'none';
+			}
+			if(!isTop){
+				clearInterval(timer);
+			}
+			isTop = false;
+		}
+
+		obtn.onclick = function(){
+			//设置定时器
+			timer = setInterval(function(){
+				//获取滚动条距离顶部的高度
+				var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+				var ispeed = Math.floor(-osTop / 6);
+				document.documentElement.scrollTop = document.body.scrollTop = osTop +ispeed;
+				isTop = true;
+				console.log(osTop -ispeed);
+				if (osTop == 0){
+					clearInterval(timer);
+				}
+			},30);
+		}
+	}
+
+
+	function getUrlParam(name){
+		var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+		var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+		if (r!=null) return unescape(r[2]); return fasle; //返回参数值
+	} 
